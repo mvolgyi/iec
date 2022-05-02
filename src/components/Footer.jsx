0d2481c0ab317131ad399278fabbf04f8f0fe43b
facku47 <86632318+facku47@users.noreply.github.com>
2022-05-01 21:52:52 -0300

    orden en el script

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
 
-        return (
-            <nav className='footercontainer'>
-                <div className='footerbox'>
-                    <div className='infowrap'>
-                    <div className='datawrapn'>
+    return (
+        <nav className='footercontainer'>
+            <div className='footerbox'>
+                <div className='infowrap'>
+                    <div className='datawrap-n'>
                         <Link to="/nosotros" className='ftext'>
                             Nosotros
                         </Link>
                     </div>
-                    <div className='datawrap '>
+                    <div className='datawrap'>
                         <Link to='/servicios' className='ftext'>
                             Servicios
                         </Link>
@@ -24,27 +24,25 @@ const Footer = () => {
                             Contacto
                         </Link>
                     </div>
-                    </div>
-                    <div className='contactwrap'>
-                        <h2 className='ctitulo'>Contacto</h2>
-                        <div className='datacwrap'>
+                </div>
+                <div className='contactwrap'>
+                    <h2 className='ctitulo'>Contacto</h2>
+                    <div className='datacwrap'>
                         <i className="fa-solid fa-phone-flip"></i>
-                            <p className='fctext'>+054 11 5758-2365</p>
-                        </div>
-                        <div className='datacwrap'>
-                        <i className="fa-solid fa-envelope-open-text"></i>
-                            <p className='fctext'>carlos.correa0470@gmail.com</p>
-                        </div>
+                        <p className='fctext'>+054 11 5758-2365</p>
                     </div>
-
+                    <div className='datacwrap'>
+                        <i className="fa-solid fa-envelope-open-text"></i>
+                        <p className='fctext'>carlos.correa0470@gmail.com</p>
+                    </div>
                 </div>
-                <div className='copyfooter'>
-                    <p className='ctext'>©Instalaciones Eléctricas Correa. {new Date().getFullYear()} Todos los derechos reservados.</p>
-
-                </div>
-            </nav>
-        )
-    }
+            </div>
+            <div className='copyfooter'>
+                <p className='ctext'>©Instalaciones Eléctricas Correa. {new Date().getFullYear()} Todos los derechos reservados.</p>
+            </div>
+        </nav>
+    )
+}
 
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbarnew = () => {
 
     const [isOpen, setIsOpen] = useState(false)
 
-    const openbar = () => {setIsOpen(!isOpen)}
+    const openbar = () => { setIsOpen(!isOpen) }
 
-    
+
     const toggleHome = () => {
         scroll.scrollToTop()
     }
@@ -36,7 +36,6 @@ const Navbarnew = () => {
     return (
 
         <nav className='navcontainer'>
-
             <div className={scrollnav ? 'navbaritems active' : 'navbaritems'}>
                 <div className={scrollnav ? 'navbarlogo active' : 'navbarlogo'} onClick={toggleHome}>IEC</div>
                 <div className='navmenu'>
@@ -46,29 +45,28 @@ const Navbarnew = () => {
                             <div className={isOpen ? 'fas fa-times' : 'fas fa-bars'} onClick={openbar}></div>
                         </div>
                     </div>
-                    <div className={isOpen ? 'bodybarmenu active' : 'bodybarmenu'}onClick={toggleHome} >
+                    <div className={isOpen ? 'bodybarmenu active' : 'bodybarmenu'} onClick={toggleHome} >
                         <div className={scrollnav ? 'navitemhome' : 'navitemhome active'} onClick={toggleHome}>
                             <Link to="/" className={scrollnav ? 'nav-links active' : 'nav-links'} onClick={isOpen}>
                                 Home
                             </Link>
                         </div>
                         <div className={scrollnav ? 'navitem active' : 'navitem'}>
-                            <Link to="/nosotros" className={scrollnav ? 'nav-links active' : 'nav-links'}onClick={isOpen}>
+                            <Link to="/nosotros" className={scrollnav ? 'nav-links active' : 'nav-links'} onClick={isOpen}>
                                 Nosotros
                             </Link>
                         </div>
                         <div className={scrollnav ? 'navitem active' : 'navitem'}>
-                            <Link to="/servicios" className={scrollnav ? 'nav-links active' : 'nav-links'}onClick={isOpen}>
+                            <Link to="/servicios" className={scrollnav ? 'nav-links active' : 'nav-links'} onClick={isOpen}>
                                 Servicios
                             </Link>
                         </div>
                         <div className={scrollnav ? 'navitem active' : 'navitem'}>
-                            <Link to="/contacto" className={scrollnav ? 'nav-links active' : 'nav-links'}onClick={isOpen}>
+                            <Link to="/contacto" className={scrollnav ? 'nav-links active' : 'nav-links'} onClick={isOpen}>
                                 Contacto
                             </Link>
                         </div>
                     </div>
-
                 </div>
             </div>
         </nav>
